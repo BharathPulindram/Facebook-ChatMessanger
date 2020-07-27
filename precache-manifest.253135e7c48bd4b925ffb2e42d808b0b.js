b05c87a9e64a87cdb0283b0ce027e2fccf218b59
Bharath Kumar <pbkbharath1707@gmail.com>
2020-07-27 16:37:35 +0530

    Updates

---
+++ precache-manifest.253135e7c48bd4b925ffb2e42d808b0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5381a7e72ee1afca1dca14afe82adf15",
+    "url": "/Facebook-clone/index.html"
+  },
+  {
+    "revision": "f02c7915f54353f3db57",
+    "url": "/Facebook-clone/static/css/main.9c979aaf.chunk.css"
+  },
+  {
+    "revision": "614ff0efb48680303a05",
+    "url": "/Facebook-clone/static/js/2.dd12b26c.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/Facebook-clone/static/js/2.dd12b26c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f02c7915f54353f3db57",
+    "url": "/Facebook-clone/static/js/main.5e77b46c.chunk.js"
+  },
+  {
+    "revision": "bec253a6eceec64b6e53",
+    "url": "/Facebook-clone/static/js/runtime-main.1168cbee.js"
+  }
+]);
